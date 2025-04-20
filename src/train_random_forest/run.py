6acd789bc077f854654bf56f24cac18a758b73f4
Mark Settergren <marksettergren@gmail.com>
2025-04-19 20:25:02 -0500

    train_random_forest fix

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -226,7 +226,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
 
     sk_pipe = Pipeline(
         steps =[
-            ("preprocessor", ColumnTransformer),
+            ("preprocessor", preprocessor),
             ("random_forest", random_forest)
         ]
     )
